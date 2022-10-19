b18f558c36dee18f8c551b8fd0029ee993182755
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2022-10-19 16:29:57 +0200

    Fixing publication of artifacts to maven repository #halcyon-64

--- halcyon-core/build.gradle.kts
+++ halcyon-core/build.gradle.kts
@@ -28,6 +28,23 @@ kotlin {
 		testRuns["test"].executionTask.configure {
 			useJUnit()
 		}
+		publishing {
+			repositories {
+				maven {
+					url = if (project.version.toString()
+							.endsWith("-SNAPSHOT", ignoreCase = true)
+					) {
+						uri(findProperty("tigaseMavenRepoSnapshot").toString())
+					} else {
+						uri(findProperty("tigaseMavenRepoRelease").toString())
+					}
+					credentials {
+						username = findProperty("mavenUsername").toString()
+						password = findProperty("mavenPassword").toString()
+					}
+				}
+			}
+		}
 	}
 	js(BOTH) {
 		browser {
