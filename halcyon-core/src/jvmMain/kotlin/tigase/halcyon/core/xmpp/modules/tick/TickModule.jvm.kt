a5d4ab243d9e7ecb5dec8552bf2e3d57cb129a3f
Wojciech Kapcia <wojtek@tigase.org>
2024-10-29 09:46:54 +0100

    Add verbose Tick logging

--- halcyon-core/src/jvmMain/kotlin/tigase/halcyon/core/xmpp/modules/tick/TickModule.jvm.kt
+++ halcyon-core/src/jvmMain/kotlin/tigase/halcyon/core/xmpp/modules/tick/TickModule.jvm.kt
@@ -2,6 +2,7 @@ package tigase.halcyon.core.xmpp.modules.tick
 
 import tigase.halcyon.core.Context
 import tigase.halcyon.core.TickEvent
+import tigase.halcyon.core.logger.LoggerFactory
 import java.util.*
 
 actual fun createTickTimer(): TickTimer = DefaultTickTimer()
@@ -14,23 +15,31 @@ class DefaultTickTimer : TickTimer {
 
     private var tickCounter: Long = 0
 
+    val logger = LoggerFactory.logger("DefaultTickTimer")
+
     override fun startTimer(context: Context) {
         tickTask = object : TimerTask() {
             override fun run() {
+                logger.finest("Firing timer, this/run: $this, task: ${tickTask}, timer: ${timer}")
                 tick(context)
             }
         }
         timer = Timer("timer", true).also {
             it.schedule(tickTask, 2_000, 2_000)
         }
+        logger.finest("Started timer, task: ${tickTask}, timer: ${timer}")
     }
 
     override fun stopTimer(context: Context) {
+        logger.finest("Stopping timer [1], task: ${tickTask}, timer: ${timer}")
         tickTask?.cancel()
+        logger.finest("Stopping timer [2], task: ${tickTask}, timer: ${timer}")
         tickTask = null
         timer?.purge()
         timer?.cancel()
+        logger.finest("Stopping timer [3], task: ${tickTask}, timer: ${timer}")
         timer = null
+        logger.finest("Stopping timer [4], task: ${tickTask}, timer: ${timer}")
     }
 
     private fun tick(context: Context) {
